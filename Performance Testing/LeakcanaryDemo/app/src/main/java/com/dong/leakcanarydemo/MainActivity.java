42a0ed6da1d7e98c71d988485f37a9c6aee0a7ac
Dong <donking0030@gmail.com>
2018-01-04 15:00:18 +0800

    Update MainActivity.java

--- Performance Testing/LeakcanaryDemo/app/src/main/java/com/dong/leakcanarydemo/MainActivity.java
+++ Performance Testing/LeakcanaryDemo/app/src/main/java/com/dong/leakcanarydemo/MainActivity.java
@@ -8,6 +8,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * 内存泄漏的几种情况：
+ * 1、集合类泄漏
+ *    当集合类是全局性的变量，只有添加元素的方法，此时集合所占用的内存只增不减，却没有相应的删除机制，对象得不到释放。从而导致内存泄漏。
+ *    Vector v = new Vector(10);
+ *    for (int i = 1; i < 100; i++) {
+ *      Object o = new Object();
+ *      v.add(o);  
+ *    }
+ * 2、单例造成的内存泄漏
+ *    
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTvStart;
